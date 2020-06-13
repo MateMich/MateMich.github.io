a34fe0cda336063b5db846103fd5cadc0589ec18
Mateusz <mateusz.michalik.poczta@gmail.com>
2020-06-13 15:44:33 +0200

    adjustments

--- src/Components/CardList.js
+++ src/Components/CardList.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import Card from './Card';
 const CardList = ({ robots }) => {
-if(true)
-{
-    throw new Error('noo :(')
-}
+
 
     const cardComponent = robots.map((user, i) => {
         return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />;
